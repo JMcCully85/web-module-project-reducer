1482e82c6deabd67996338dd2bd01ff7524227ab
jeremy mccully <j_mccully85@yahoo.com>
2021-09-15 21:43:27 -0800

    completed the project

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,9 @@
 # Understanding Questions:
-1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
-* The user presses the 1 button.
-* 
-...
 
-* TotalDisplay shows the total plus 1.
+What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
+The user presses the 1 button.
+On click calls handler function
+handler function calls dispatch with addOne() which is of type ADD_ONE
+the ADD_ONE case then returns sate with the change of total being incremented
+the change of state rerenders the component
+TotalDisplay shows the total plus 1.
--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,25 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const APPLY_MEMORY = "APPLY_MEMORY";
 
 export const addOne = () => {
-    return({type:ADD_ONE});
-}
+  return { type: ADD_ONE };
+};
 
 export const applyNumber = (number) => {
-    return({type:APPLY_NUMBER, payload:number});
-}
+  return { type: APPLY_NUMBER, payload: number };
+};
+
+export const changeOperation = (operation) => {
+  return { type: CHANGE_OPERATION, payload: operation };
+};
+
+export const applyMemory = (action) => {
+  return { type: APPLY_MEMORY, payload: action };
+};
+
+export const clearDisplay = () => {
+  return { type: CLEAR_DISPLAY };
+};
